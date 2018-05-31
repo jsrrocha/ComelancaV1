a5cac5f983aa4b073a855ae64b3869f302a927eb
asgoveia <amanda.asg95@gmail.com>
2018-05-31 16:14:02 -0300

    Restaurant List

--- app/src/main/java/com/example/jessica/comelancav1/ListRestaurantActivity.java
+++ app/src/main/java/com/example/jessica/comelancav1/ListRestaurantActivity.java
@@ -2,12 +2,30 @@ package com.example.jessica.comelancav1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListRestaurantActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_restaurant);
+        ListView listaRestaurantes = (ListView)findViewById(R.id.lista);
+
+        List<String> restaurantes = new ArrayList<String>();
+        restaurantes.add("Restaurante 1");
+        restaurantes.add("Restaurante 2");
+        restaurantes.add("Restaurante 3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, restaurantes);
+
+        listaRestaurantes.setAdapter(adapter);
     }
 }
+
