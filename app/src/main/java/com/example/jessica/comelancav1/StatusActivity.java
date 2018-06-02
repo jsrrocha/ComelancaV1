5459f8602927ba2c7c26d70bf736414a5b30da3c
asgoveia <amanda.asg95@gmail.com>
2018-06-01 21:46:21 -0300

    Added status files

---
+++ app/src/main/java/com/example/jessica/comelancav1/StatusActivity.java
@@ -0,0 +1,20 @@
+package com.example.jessica.comelancav1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+        TextView texto = (TextView) findViewById(R.id.texto);
+
+        new MinhaTask(this, progress, texto).execute();
+    }
+}
