0c9cc58a8782d782fe6b9306cdf918a8c6ae8e30
asgoveia <amanda.asg95@gmail.com>
2018-06-02 20:45:28 -0300

    Completando o endereço pelo cep

--- app/src/main/java/com/example/jessica/comelancav1/MainActivity.java
+++ app/src/main/java/com/example/jessica/comelancav1/MainActivity.java
@@ -2,19 +2,31 @@ package com.example.jessica.comelancav1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.jessica.comelancav1.domain.Util;
+import com.example.jessica.comelancav1.domain.ZipCodeListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static EditText etZipCode;
+    private static String cep;
+    private Util util;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("ComeLança");
+
+        etZipCode = (EditText) findViewById(R.id.textCep);
+
     }
     public void openAddressActivity(View view) {
         Intent intent = new Intent(this, AddressActivity.class);
+        intent.putExtra("CEP", etZipCode.getText().toString());
         startActivity(intent);
     }
 
@@ -27,4 +39,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
 }
