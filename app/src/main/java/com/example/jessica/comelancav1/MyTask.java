4209df98258973fc008f268709084bfb55bcdcae
Jéssica Salvador Rodrigues da Rocha <jessyksrrocha@hotmail.com>
2018-06-30 22:10:54 -0300

    status show, change backgroud and more fixes

--- app/src/main/java/com/example/jessica/comelancav1/MyTask.java
+++ app/src/main/java/com/example/jessica/comelancav1/MyTask.java
@@ -2,61 +2,69 @@ package com.example.jessica.comelancav1;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.view.Gravity;
-import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.kofigyan.stateprogressbar.StateProgressBar;
 
 public class MyTask extends AsyncTask<Object, Object, String> {
 
-    private ProgressBar progressBar;
-    private TextView texto;
-    String[] status = {"Aceito pelo restaurante", "Em preparo", "Saiu para entrega", "Entrega realizada"};
+    private StateProgressBar progressBar;
+    private TextView textTimeTo;
+    private TextView textTime;
 
-    private int total = 0;
-    private static int PROGRESSO = 25;
+    private TextView textViewH;
+    private int total = 1;
 
-    public MyTask(Context context, ProgressBar progressBar, TextView texto) {
+
+    public MyTask(Context context, StateProgressBar progressBar,TextView textTimeTo,TextView textTime,TextView textViewH) {
         this.progressBar = progressBar;
-        this.texto = texto;
-    }
+        this.textTimeTo = textTimeTo;
+        this.textTime = textTime;
+        this.textViewH = textViewH;
 
-   /* protected void onPreExecute() {
-        texto.setText(status[0]);
-        super.onPreExecute();
-    }*/
+    }
 
     @Override
     protected String doInBackground(Object... params) {
         try {
-
-            Thread.sleep(1500);
-
+            //Thread.sleep(1500);
             for (int i=0; i<4; i++) {
-                publishProgress();
                 Thread.sleep(4000);
+                total = total + 1;
+                publishProgress();
             }
 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
     @Override
     protected void onProgressUpdate(Object... values) {
 
-        total += PROGRESSO;
-        int i = (total/25) - 1;
-        progressBar.incrementProgressBy(PROGRESSO);
-        texto.setText(status[i]);
-        texto.setGravity(Gravity.CENTER_HORIZONTAL);
+        switch (total) {
+            case 2:
+                 progressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                 textViewH.setText("Em preparo às 21:15");
+                 textTime.setText("25 minutos");
+
+                 break;
+            case 3:
+                 progressBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE);
+                 textViewH.setText("Saiu para entrega às 21:40");
+                 textTime.setText("15 minutos");
+                 break;
+            case 4:
+                 progressBar.setCurrentStateNumber(StateProgressBar.StateNumber.FOUR);
+                 textViewH.setText("Boa refeição :)");
+                 textTime.setText("");
+                 textTimeTo.setText("");
+                 break;
+            default:
+                 progressBar.setCurrentStateNumber(StateProgressBar.StateNumber.FOUR);
+        }
         super.onProgressUpdate(values);
     }
 
-   /* @Override
-    protected void onPostExecute(String result) {
-        progressBar.setVisibility(ProgressBar.INVISIBLE);
-        super.onPostExecute(result);
-    }*/
 }
--- app/src/main/java/com/example/jessica/comelancav1/StatusActivity.java
+++ app/src/main/java/com/example/jessica/comelancav1/StatusActivity.java
@@ -1,20 +1,63 @@
 package com.example.jessica.comelancav1;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.kofigyan.stateprogressbar.StateProgressBar;
+
 public class StatusActivity extends AppCompatActivity {
+
+    String[] status = {"Aceito\npelo\nrestaurante", "Em\npreparo", "Saiu\npara\nentrega", "Entrega\nrealizada"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
-        ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
-        TextView texto = (TextView) findViewById(R.id.texto);
+        TextView textTime = (TextView) findViewById(R.id.textTime);
+        TextView textTimeTo = (TextView) findViewById(R.id.textViewTimeTo);
+        TextView textViewH = (TextView) findViewById(R.id.textViewHours);
 
-        new MyTask(this, progress, texto).execute();
+
+        StateProgressBar stateProgressBar = (StateProgressBar) findViewById(R.id.your_state_progress_bar_id);
+        stateProgressBar.setStateDescriptionData(status);
+
+        stateProgressBar.setForegroundColor(ContextCompat.getColor(this, android.R.color.holo_green_dark));
+        stateProgressBar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray));
+
+        stateProgressBar.setStateNumberForegroundColor(ContextCompat.getColor(this, android.R.color.white));
+        stateProgressBar.setStateNumberBackgroundColor(ContextCompat.getColor(this, android.R.color.background_dark));
+
+        stateProgressBar.setStateSize(40f);
+        stateProgressBar.setStateNumberTextSize(20f);
+        stateProgressBar.setStateLineThickness(10f);
+
+        stateProgressBar.checkStateCompleted(true);
+
+        stateProgressBar.enableAnimationToCurrentState(true);
+        stateProgressBar.setAnimationStartDelay(2000);
+        stateProgressBar.setAnimationDuration(3000);
+
+        stateProgressBar.setDescriptionTopSpaceIncrementer(10f);
+        stateProgressBar.setStateDescriptionSize(16f);
+
+        stateProgressBar.setCurrentStateDescriptionColor(ContextCompat.getColor(this, android.R.color.holo_green_dark));
+        stateProgressBar.setStateDescriptionColor(ContextCompat.getColor(this,  android.R.color.holo_green_dark));
+
+        stateProgressBar.setStateDescriptionTypeface("fonts/RobotoSlab-Light.ttf");
+        stateProgressBar.setStateNumberTypeface("fonts/Questrial-Regular.ttf");
+
+        stateProgressBar.setMaxDescriptionLine(3);
+        stateProgressBar.setJustifyMultilineDescription(false);
+        stateProgressBar.setDescriptionLinesSpacing(10f);
+
+        stateProgressBar.setStateNumberIsDescending(false);
+
+        //ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+       // TextView texto = (TextView) findViewById(R.id.texto);
+
+        new MyTask(this, stateProgressBar,textTimeTo,textTime,textViewH).execute();
     }
 }
